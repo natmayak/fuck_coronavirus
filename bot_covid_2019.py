56ade8584dca2f85214b73c027dd3fab80f17a25
YuryProkhorov <5071177@gmail.com>
2020-04-12 15:26:05 +0300

    subscribers done

--- bot_covid_2019.py
+++ bot_covid_2019.py
@@ -10,13 +10,15 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO, filename='bot.log')
 
+#Создаем сет с подписчиками. Отсюда будем забирать чат айди
+subscribers = set()
 
 def greeting(update, context):
     user = update.effective_user
     first_name = user.first_name
     emo = emojize(choice(settings.USER_EMOJI), use_aliases=True)
     # todo Добавить инструкцию и описание бота типа что бот делает, как и что делать пользователю
-    greeting_text = f'Hello {first_name}{emo}. Best of luck with the corona crisis. I am going to help you stay alive. Try to keep your hands off anyway'
+    greeting_text = f'Hello {first_name}{emo}. Best of luck with the corona crisis. I am going to help you to stay alive. Try to keep your hands off anyway. Now try /subscribe and see what happens!'
     context.bot.send_message(chat_id=update.effective_chat.id, text=greeting_text, reply_markup=get_keyboard())
     print(update.effective_chat.id)
 
@@ -66,14 +68,14 @@ def regular_messages(context):
     action_set = [["Do not touch your face!", 'Well I do not touch it!', 'media/face.tgs', "But it's okay to touch yourself:)"],
                 ["Wash your hands!", 'Alright, alright. I have washed my hands!', 'media/hands.tgs', 'Enjoy your sparkling fingers then'],
                 ["Open your windows and go out of your room for 10-15 minutes", 'Hm ok, I will do that.', 'media/fine.tgs', 'Now breathe in deeply through the nose'],
-                ["Fuck coronavirus!", 'Hell yeah! Fuck it!!!', 'media/fuck.tgs', 'You are supposed to get a busted picture of covid that I do not have so far']]
+                ["Fuck coronavirus!", 'Hell yeah! Fuck it!!!', 'media/fuck.tgs', 'Busted picture of covid that I do not have so far']]
     action = choice(action_set)
     keyboard = [[InlineKeyboardButton(action[1], callback_data=action[3])]]
     reply_button = InlineKeyboardMarkup(keyboard)
     #sticker = choice(glob(str(action[2])))
-    #todo разобраться с chat id
-    #context.bot.send_sticker(chat_id=165436471, sticker=open(sticker, 'rb'))
-    context.bot.send_message(chat_id=165436471, text=action[0], reply_markup=reply_button)
+    for chat_id in subscribers:
+        #context.bot.send_sticker(chat_id=chat_id, sticker=open(sticker, 'rb'))
+        context.bot.send_message(chat_id=chat_id, text=action[0], reply_markup=reply_button)
 
 
 def button(update, context):
@@ -81,6 +83,25 @@ def button(update, context):
     query.answer()
     #todo нужно еще удалять стикеры.
     query.edit_message_text(text='{}'.format(query.data))
+
+
+def subscribe(update, context):
+    subscribers.add(update.message.chat_id)
+    subscribe_text = "Hey thanks for your subscribing! If you are not good enought for us you can always use /unsubscribe. But better do not. Please?"
+    for chat_id in subscribers:
+        context.bot.send_message(chat_id=chat_id, text=subscribe_text)
+
+
+def unsubscribe(update, context):
+    unsubscribe_text = "Well you are gonna die anyway. Little pussy! See you..."
+    nonsubscribe_text = "And how you can unsubscribe if you are not subscribed yet? Use your brain and press /subscribe"
+    if update.message.chat_id in subscribers:
+        for chat_id in subscribers:
+            context.bot.send_message(chat_id=chat_id, text=unsubscribe_text)
+            subscribers.remove(update.message.chat_id)
+    else:
+        chat_id = update.message.chat_id
+        context.bot.send_message(chat_id=chat_id, text=nonsubscribe_text)
 
 
 def main():
@@ -94,6 +115,8 @@ def main():
     dp.add_handler(MessageHandler(Filters.location, get_location))
     updater.job_queue.run_repeating(regular_messages, interval=10, first=10)
     dp.add_handler(CallbackQueryHandler(button))
+    dp.add_handler(CommandHandler('subscribe', subscribe))
+    dp.add_handler(CommandHandler('unsubscribe', unsubscribe))
 
     updater.start_polling()
     updater.idle()
