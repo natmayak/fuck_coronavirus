73a41713d9386dd18f336795a0b9cc5b7495b9a3
YuryProkhorov <5071177@gmail.com>
2020-04-16 17:14:16 +0300

    changed API KEY to coronavirus bot

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
          'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
-# API_KEY = '1168775828:AAHrIux7S3MObQy2qCGo2kvVT1oeHCOTBO4'
-API_KEY = '1097577104:AAHoqylJFKEK7HDsanQNHTYW5qtSM-2rjqo'
+API_KEY = '1168775828:AAHrIux7S3MObQy2qCGo2kvVT1oeHCOTBO4'
+# API_KEY = '1097577104:AAHoqylJFKEK7HDsanQNHTYW5qtSM-2rjqo'
 # pip3 install python-telegram-bot[socks]==12.2.0
 # pip3 install emoji
 USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':heart:', ':fire:', ':rocket:', ':earth_americas:']
