d0ecacefcd2574c347caaebb0f9827b13f9d9487
YuryProkhorov <5071177@gmail.com>
2020-04-22 14:24:55 +0300

    subscribe/unsubscribe buttons added. But they do not change when user subscribes/unsubscribes

--- bot_covid_2019.py
+++ bot_covid_2019.py
@@ -10,8 +10,9 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO, filename='bot.log')
 
-#Создаем сет с подписчиками. Отсюда будем забирать чат айди
+# Создаем сет с подписчиками. Отсюда будем забирать чат айди
 subscribers = set()
+
 
 def greeting(update, context):
     user = update.effective_user
@@ -19,28 +20,32 @@ def greeting(update, context):
     emo = emojize(choice(settings.USER_EMOJI), use_aliases=True)
     # todo Добавить инструкцию и описание бота типа что бот делает, как и что делать пользователю
     greeting_text = f'Hello {first_name}{emo}. Best of luck with the corona crisis. I am going to help you to stay alive. Try to keep your hands off anyway. Now try /subscribe and see what happens!'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=greeting_text, reply_markup=get_keyboard())
+    context.bot.send_message(chat_id=update.effective_chat.id, text=greeting_text, reply_markup=get_keyboard(update))
     print(update.effective_chat.id)
+
+
+def get_keyboard(update):
+    location_button = KeyboardButton('location', request_location=True)
+    if update.message.chat_id in subscribers:
+        sub_button = 'unsubscribe'
+    else:
+        sub_button = 'subscribe'
+    buttons = ReplyKeyboardMarkup([['I want to go out', sub_button], [location_button]], resize_keyboard=True)
+    return buttons
 
 
 def talk_to_me(update, context):
     emo = emojize(choice(settings.USER_EMOJI), use_aliases=True)
     user_text = "Hi {} {}, you just caught some germs and said {}".format(update.message.chat.first_name, emo,
                                                                           update.message.text)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=user_text, reply_markup=get_keyboard())
+    context.bot.send_message(chat_id=update.effective_chat.id, text=user_text, reply_markup=get_keyboard(update))
 
 
 def get_location(update, context):
     print(update.message.location)
     emo = emojize(choice(settings.USER_EMOJI), use_aliases=True)
     location_text = f'Big brother is watching you (for your own safety) {emo}'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=location_text, reply_markup=get_keyboard())
-
-
-def get_keyboard():
-    location_button = KeyboardButton('location', request_location=True)
-    buttons = ReplyKeyboardMarkup([['I want to go out', 'sticker'], [location_button]], resize_keyboard=True)
-    return buttons
+    context.bot.send_message(chat_id=update.effective_chat.id, text=location_text, reply_markup=get_keyboard(update))
 
 
 def send_covid(update, context):
@@ -60,10 +65,14 @@ def brodsky(update, context):
 
 
 def regular_messages(context):
-    action_set = [["Do not touch your face!", 'Well I do not touch it!', 'media/face.tgs', "But it's okay to touch yourself:)"],
-                  ["Wash your hands!", 'Alright, alright. I have washed my hands!', 'media/hands.tgs', 'Enjoy your sparkling fingers then'],
-                  ["Open your windows and go out of your room for 10-15 minutes", 'Well ok, I will do that.', 'media/fine.tgs', 'Now breathe in deeply through the nose'],
-                  ["Fuck coronavirus!", 'Hell yeah! Fuck it!!!', 'media/fuck.tgs', 'Busted picture of covid that I do not have so far']]
+    action_set = [
+        ["Do not touch your face!", 'Well I do not touch it!', 'media/face.tgs', "But it's okay to touch yourself:)"],
+        ["Wash your hands!", 'Alright, alright. I have washed my hands!', 'media/hands.tgs',
+         'Enjoy your sparkling fingers then'],
+        ["Open your windows and go out of your room for 10-15 minutes", 'Well ok, I will do that.', 'media/fine.tgs',
+         'Now breathe in deeply through the nose'],
+        ["Fuck coronavirus!", 'Hell yeah! Fuck it!!!', 'media/fuck.tgs',
+         'Busted picture of covid that I do not have so far']]
     action = choice(action_set)
     keyboard = [[InlineKeyboardButton(action[1], callback_data=action[3])]]
     reply_button = InlineKeyboardMarkup(keyboard)
@@ -80,27 +89,31 @@ def regular_messages(context):
 def button(update, context):
     query = update.callback_query
     query.answer()
-    #todo нужно еще удалять стикеры.
+    # todo нужно еще удалять стикеры.
     query.edit_message_text(text='{}'.format(query.data))
 
 
 def subscribe(update, context):
-    subscribers.add(update.message.chat_id)
     subscribe_text = "Hey thanks for your subscribing! If you are not good enought for us you can always use /unsubscribe. But better do not. Please?"
-    for chat_id in subscribers:
-        context.bot.send_message(chat_id=chat_id, text=subscribe_text)
+    already_subscribe_text = "Oh please stop it! You are already such a good boy/girl."
+    if update.message.chat_id in subscribers:
+        context.bot.send_message(chat_id=update.message.chat_id, text=already_subscribe_text,
+                                 reply_markup=get_keyboard(update))
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text=subscribe_text, reply_markup=get_keyboard(update))
+        subscribers.add(update.message.chat_id)
 
 
 def unsubscribe(update, context):
     unsubscribe_text = "Well you are gonna die anyway. Little pussy! See you..."
     nonsubscribe_text = "And how you can unsubscribe if you are not subscribed yet? Use your brain and press /subscribe"
     if update.message.chat_id in subscribers:
-        for chat_id in subscribers:
-            context.bot.send_message(chat_id=chat_id, text=unsubscribe_text)
-            subscribers.remove(update.message.chat_id)
+        context.bot.send_message(chat_id=update.message.chat_id, text=unsubscribe_text,
+                                 reply_markup=get_keyboard(update))
+        subscribers.remove(update.message.chat_id)
     else:
-        chat_id = update.message.chat_id
-        context.bot.send_message(chat_id=chat_id, text=nonsubscribe_text)
+        context.bot.send_message(chat_id=update.message.chat_id, text=nonsubscribe_text,
+                                 reply_markup=get_keyboard(update))
 
 
 def main():
@@ -109,10 +122,11 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start', greeting))
     dp.add_handler(MessageHandler(Filters.regex('^(I want to go out)$'), brodsky))
-    dp.add_handler(MessageHandler(Filters.regex('^(sticker)$'), send_covid))
+    dp.add_handler(MessageHandler(Filters.regex('^(subscribe)$'), subscribe))
+    dp.add_handler(MessageHandler(Filters.regex('^(unsubscribe)$'), unsubscribe))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     dp.add_handler(MessageHandler(Filters.location, get_location))
-    updater.job_queue.run_repeating(regular_messages, interval=10, first=10)
+    updater.job_queue.run_repeating(regular_messages, interval=20, first=10)
     dp.add_handler(CallbackQueryHandler(button))
     dp.add_handler(CommandHandler('subscribe', subscribe))
     dp.add_handler(CommandHandler('unsubscribe', unsubscribe))
@@ -122,8 +136,3 @@ def main():
 
 
 main()
-
-
-
-
-
