7db5b9dcac25e5db00c1c42d26fc9221c0cea5e2
YuryProkhorov <5071177@gmail.com>
2020-04-29 20:05:41 +0300

    settings rescued

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
          'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
 API_KEY = '1097577104:AAHoqylJFKEK7HDsanQNHTYW5qtSM-2rjqo'
+API_KEY = '1168775828:AAHrIux7S3MObQy2qCGo2kvVT1oeHCOTBO4'
+
 
 USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':heart:', ':fire:', ':rocket:', ':earth_americas:']
 
