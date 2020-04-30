97036762f13c0fa0d163a30e93c7fb2d7fdf48ac
5071177 <5071177@gmail.com>
2020-04-30 12:37:25 +0300

    seetings changed after requirements creation

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
          'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
-#API_KEY = '1097577104:AAHoqylJFKEK7HDsanQNHTYW5qtSM-2rjqo'
-API_KEY = '1168775828:AAHrIux7S3MObQy2qCGo2kvVT1oeHCOTBO4'
+API_KEY = '1097577104:AAHoqylJFKEK7HDsanQNHTYW5qtSM-2rjqo'
+#API_KEY = '1168775828:AAHrIux7S3MObQy2qCGo2kvVT1oeHCOTBO4'
 
 
 USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':heart:', ':fire:', ':rocket:', ':earth_americas:']
@@ -11,6 +11,3 @@ USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':heart:', ':fire
 
 API_KEY_YNDX='93e1e8d5-57ae-48c5-b78a-55a2f2e3f661'
 
-# pip3 install python-telegram-bot[socks]==12.2.0
-# pip3 install emoji
-# pip3 install requests
