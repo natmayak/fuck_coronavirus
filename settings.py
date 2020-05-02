5d2daf167e68adb3c3d41f7c4460868921c0fbd9
5071177 <5071177@gmail.com>
2020-05-02 14:10:44 +0300

    settings cleared

--- settings.py
+++ settings.py
@@ -1,15 +1,10 @@
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
          'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
-#API_KEY = '1097577104:AAHoqylJFKEK7HDsanQNHTYW5qtSM-2rjqo' - NN
-API_KEY = '1168775828:AAHrIux7S3MObQy2qCGo2kvVT1oeHCOTBO4' #- Main
-#API_KEY = '1298916653:AAE2g4igkCVl7c7_1WnfibU7gO5JJNcC_-o' #- YP
-
-
+API_KEY = '1168775828:AAHrIux7S3MObQy2qCGo2kvVT1oeHCOTBO4'
 
 USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':heart:', ':fire:', ':rocket:', ':earth_americas:']
 
-# API_KEY_YNDX = 'ed24674c-a6bc-4c61-9ce0-08615315b23a'
 API_KEY_YNDX='93e1e8d5-57ae-48c5-b78a-55a2f2e3f661'
 
 MONGO_LINK = 'mongodb+srv://Admin:fuckcorona1234@fuckcoronabot-nnrxw.mongodb.net/Users?retryWrites=true&w=majority'
