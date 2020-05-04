0062573ab4770e920e67464f27f4d66e3d5b41ad
5071177 <5071177@gmail.com>
2020-05-04 11:51:23 +0300

    APIs hidden

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
          'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
-API_KEY = '1168775828:AAHrIux7S3MObQy2qCGo2kvVT1oeHCOTBO4'
+API_KEY = '<TelegramBot API key>'
 
 USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':heart:', ':fire:', ':rocket:', ':earth_americas:']
 
-API_KEY_YNDX='93e1e8d5-57ae-48c5-b78a-55a2f2e3f661'
+API_KEY_YNDX='<Yandex API key>'
 
-MONGO_LINK = 'mongodb+srv://Admin:fuckcorona1234@fuckcoronabot-nnrxw.mongodb.net/Users?retryWrites=true&w=majority'
+MONGO_LINK = '<Mongo Link>'
 
 MONGO_DB = 'Users'
